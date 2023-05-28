6ebcfce0b6c2f9873b1f913d7ffcad69df340f29
Kayro César <39713656+kayrocesar@users.noreply.github.com>
2023-05-28 15:15:56 -0300

    menu principal criado

---
+++ src/menu.pl
@@ -0,0 +1,53 @@
+:- use_module(library(pce)).
+:- [src/ano].
+
+menu:-
+    new(Dialog, dialog('IMDb Recommendation')),
+    send(Dialog, size, size(500, 500)),
+    send(Dialog, background, '#87CEEB'),
+    send_list(Dialog,
+              append,
+              [
+                new(S, new(S, menu('Escolha um criterio'))),
+                button(cancelar, message(Dialog, destroy)),
+                button(confirmar,
+                       and(message(@prolog,
+                                   menu_escolha,
+                                   S?selection
+                                   )))
+              ]),
+    send_list(S,
+              append,
+
+              [ 'Ano',
+                'Duração',
+                'Gênero',
+                'Classificação',
+                'Diretor',
+                'Arrecadação'
+              ]),
+    send(S, columns, 2),
+    send(S, colour('#000000')),
+    send(S, gap, size(15, 15)),
+    send(Dialog, default_button, confirmar),
+    send(Dialog, open_centered).
+
+
+menu_escolha(Option) :-
+    write(Option),
+    (   Option = 'Ano'
+    ->  menu_rec_ano
+    ;   Option = 'Duração'
+    ->  menu_rec_ano
+    ;   Option = 'Gênero'
+    ->  menu_rec_ano
+    ;   Option = 'Classificação'
+    ->  menu_rec_ano
+    ;   Option = 'Diretor'
+    ->  menu_rec_ano
+    ;   Option = 'Arrecadação'
+    ->  menu_rec_ano
+    ;   menu
+    ).
+
+
