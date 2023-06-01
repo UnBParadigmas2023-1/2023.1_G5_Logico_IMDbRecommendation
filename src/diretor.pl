987dfdf4639ea84202df1a893781ea61c0cff754
Lucas Lopes <38164895+lucaslop@users.noreply.github.com>
2023-05-31 21:56:27 -0300

    Update diretor.pl
    Co-authored-by: Thalisson Alves <thalisson2030@gmail.com>
    Co-authored-by: Flavio Vieira <31935777+flaviovl@users.noreply.github.com>
    Co-authored-by: Kayro César <39713656+kayrocesar@users.noreply.github.com>
    Co-authored-by: Lucas Lopes <38164895+lucaslop@users.noreply.github.com>
    Co-authored-by: 8ifq3 <github.8ifq3@slmails.com>
    Co-authored-by: Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
    Co-authored-by: Paulo Gonçalves Lima <18203100+PauloGoncalvesLima@users.noreply.github.com>
    Co-authored-by: Mateus Maia <mateusmaiamaia@hotmail.com>
    Co-authored-by: luisgfmarques <luisgustavomarques@outlook.com>

--- src/diretor.pl
+++ src/diretor.pl
@@ -1,26 +1,31 @@
+:- [src/database/data].
+:- (dynamic filme/11, diretor/1).
+
 menu_rec_diretor :-
-    new(Dialog, dialog('Recomendacao por Diretor')),
+    new(Dialog, dialog('Recomendacao por Diretores')),
     send(Dialog, size, size(500, 500)),
-    send(Dialog, background, '#23617a'),
+    send(Dialog, background, '#87CEEB'),
     send_list(Dialog,
               append,
-
-              [ new(diretor, slider('Diretor:', Frank Darabont, Steven Spielberg, Jonathan Demme)),
+              
+              [ new(Diretor, menu(diretor, cycle)),
                 button(cancelar, message(Dialog, destroy)),
                 button(confirmar,
                        and(message(@prolog,
                                    rec_diretor,
-                                   diretor?selection),
+                                   Diretor?selection),
                            message(Dialog, destroy)))
               ]),
-    send(diretor, colour('#000000')),
+    forall(diretor(G), send_list(Diretor, append, G)),
+    send(Diretor, colour('#000000')),
     send(Dialog, open_centered).
 
 
-rec_diretor(diretor) :-
+rec_(Diretor) :-
     new(D, dialog('Filmes Recomendados')),
     findall(Filme,
-            ( filme(_,_,Filme,Diretor,_,_,_)
+            ( filme(Filme,_,_,_,_,Diretor,_,_,_,_,_)
+              
             ),
             Filmes),
     append_text_dialog(Filmes, D),
